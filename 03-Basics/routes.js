bee3a26b6af88197be633414e6cf3a173e572c2e
xyfka <lukasz.maz@gmail.com>
2020-09-29 19:12:29 +0100

    Debugger

--- 03-Basics/routes.js
+++ 03-Basics/routes.js
@@ -8,7 +8,7 @@ const requestHandler = (req, res) => {
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send!</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send!!!</button></form></body>');
         res.write('</html>');
         return res.end();
         }
