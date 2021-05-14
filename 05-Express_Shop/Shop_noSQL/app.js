61be560baf00242e47960d82d300013abbc2bdff
xyfka <lukasz.maz@gmail.com>
2021-05-14 13:04:54 +0100

    update

--- 05-Express_Shop/Shop_noSQL/app.js
+++ 05-Express_Shop/Shop_noSQL/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -12,27 +13,25 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user;
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
-    next();
+  User.findById('60997fd16e8a7f923bedc66d')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoConnect(() => {
-    
-    app.listen(3000);
-})
+  app.listen(3000);
+});
--- 05-Express_Shop/Shop_noSQL/util/database.js
+++ 05-Express_Shop/Shop_noSQL/util/database.js
@@ -2,24 +2,27 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let _db;
-const mongoConnect = (cb) => {
-    MongoClient.connect('mongodb+srv://xyfka:KrB5lDNMddLb7apQ@cluster0.cumre.mongodb.net/shop?retryWrites=true&w=majority', { 
-        useUnifiedTopology: true })
-.then(client => {
-    console.log("Database Connected!");
-    _db = client.db();
-    cb();
-})
-.catch(err => {console.log(err);
-    throw err;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://xyfka:KrB5lDNMddLb7apQ@cluster0.cumre.mongodb.net/shop?retryWrites=true&w=majority', {  useUnifiedTopology: true })
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
     });
 };
 
-const getDB = () => {
-    if (_db) {
-        return _db;
-    }
-    throw 'No database found!';
-}
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
 exports.mongoConnect = mongoConnect;
-exports.getDB = getDB;
+exports.getDb = getDb;
